325130f580c21393a42f900e8e6d28b45146c5d3
BazsiHorvath <balazs@spoor.ai>
2023-12-29 14:39:04 +0200

    added test for label transform

--- projects/skin-burn-segmentation/tests/test_data_preprocessing.py
+++ projects/skin-burn-segmentation/tests/test_data_preprocessing.py
@@ -1,16 +1,16 @@
 from skinburnsegmentation.data_preprocessing.transforms import (
-    get_image_transform, get_label_transform)
+    get_image_transform,
+    get_label_transform
+)
+from skinburnsegmentation.constants import CLASS_NAME_ID_MAP
 
 
 def test_get_image_transform_gets_train_transform():
-    # Given
     input_size = (256, 256)
     is_train = True
 
-    # When
     image_transform = get_image_transform(input_size, is_train)
 
-    # Then
     assert image_transform.transforms[0].__class__.__name__ == "Resize"
     assert image_transform.transforms[1].__class__.__name__ == "RandomHorizontalFlip"
     assert image_transform.transforms[2].__class__.__name__ == "RandomRotation"
@@ -20,16 +20,18 @@ def test_get_image_transform_gets_train_transform():
     assert image_transform.transforms[0].size == input_size
 
 def test_get_image_transform_gets_not_train_transform():
-    # Given
     input_size = (256, 256)
     is_train = False
 
-    # When
     image_transform = get_image_transform(input_size, is_train)
 
-    # Then
     assert image_transform.transforms[0].__class__.__name__ == "Resize"
     assert image_transform.transforms[1].__class__.__name__ == "ToTensor"
     assert image_transform.transforms[2].__class__.__name__ == "Normalize"
 
     assert image_transform.transforms[0].size == input_size
+
+def test_label_transform():
+    label_transform = get_label_transform()
+    for key, value in CLASS_NAME_ID_MAP.items():
+        assert label_transform(key) == value
